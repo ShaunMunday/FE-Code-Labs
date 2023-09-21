3c385f69d95208e187951852019d05b1b445f81a
ShaunMunday <138719192+ShaunMunday@users.noreply.github.com>
2023-09-21 03:45:59 -0500

    Add files via upload

---
+++ JS-9:18/Ex.1/main.js
@@ -0,0 +1,14 @@
+document.getElementById("myDiv").style.backgroundColor = "lightblue";
+
+let text = document.getElementById("p1").innerText;
+document.getElementById("p1").innerHTML = text;
+console.log(text)
+
+let text2 = document.getElementById("p2").innerText;
+document.getElementById("p2").innerHTML = text2;
+console.log(text2)
+
+
+
+
+
